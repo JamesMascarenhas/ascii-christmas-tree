59c76bf2d94156fffeb3f0df82a4737eae78072b
James Mascarenhas <james.mas27@gmail.com>
2024-12-27 01:09:38 -0500

    Update xmas_tree.sh

--- xmas_tree.sh
+++ xmas_tree.sh
@@ -44,7 +44,7 @@ EOF
 TREE_HEIGHT=18
 
 MESSAGE=(
-	"${color_lights[3]}Merry Christmas Edith!"
+	"${color_lights[3]}Merry Christmas Noodles!"
 	"${color_lights[3]}From James"
 	"${color_lights[1]}$ ${color_lights[0]}curl jamesmascarenhas.sh"
 )
