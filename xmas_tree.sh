3258d8f5a4441485b90f8fb449607933e89f1b25
James Mascarenhas <james.mas27@gmail.com>
2024-12-27 04:14:56 -0500

    Update xmas_tree.sh

--- xmas_tree.sh
+++ xmas_tree.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 #
-# Create a christmas tree with many twinkling stars on the terminal
+# Create a christmas tree on the terminal
 #
 # Author: Dave Eddy <dave@daveeddy.com>
 # Modified by: James Mascarenhas
@@ -9,9 +9,6 @@
 # colors
 color_tree=$(tput setaf 2)
 color_star=$(tput setaf 227)   # Golden star
-color_twinkle=$(tput setaf 15)  # Bright white for twinkling stars
-
-# lights - dave matched these to "vintaglo vintage christmas lights"
 color_lights=(
 	"$(tput setaf 111)"
 	"$(tput setaf 208)"
@@ -63,40 +60,12 @@ COLS=$(tput cols)
 LINES=$(tput lines)
 middle_y=$((LINES / 2 - (TREE_HEIGHT / 2)))
 
-# dynamically generate random star positions around the tree
-generate_star_positions() {
-    star_positions=()
-    for _ in {1..150}; do  # Generate 150 stars
-        while true; do
-            rand_y=$((RANDOM % LINES))  # Random Y position within terminal height
-            rand_x=$((RANDOM % COLS))   # Random X position within terminal width
-
-            # Exclude the tree region
-            if ((rand_y >= middle_y && rand_y <= middle_y + TREE_HEIGHT)) &&
-               ((rand_x >= COLS / 2 - 10 && rand_x <= COLS / 2 + 10)); then
-                continue
-            fi
-
-            # Exclude the message region
-            if ((rand_y >= middle_y + TREE_HEIGHT && rand_y <= middle_y + TREE_HEIGHT + 3)) &&
-               ((rand_x >= COLS / 2 - 10 && rand_x <= COLS / 2 + 30)); then
-                continue
-            fi
-
-            # Add star position if valid
-            star_positions+=("$rand_y $rand_x")
-            break
-        done
-    done
-}
-
 # current color index
-generate_star_positions
 idx=0
 while true; do
 	# stylize and colorize tree
 	t=$color_tree$TREE
-	t=${t// \*/ ${color_star}*${color_tree} }  # Golden star at the top stays fixed
+	t=${t// \*/ ${color_star}*${color_tree} }  # Golden star at the top
 	t=${t// 0 / ${color_lights[idx % len]}o${color_tree} }
 	t=${t// 1 / ${color_lights[(idx + 1) % len]}o${color_tree} }
 	t=${t// 2 / ${color_lights[(idx + 2) % len]}o${color_tree} }
@@ -105,22 +74,6 @@ while true; do
 	# display the tree
 	tput cup "$middle_y" $((COLS / 2 - 10))
 	echo "$t"
-
-	# display the twinkling stars
-	for pos in "${star_positions[@]}"; do
-		star_y=$(echo "$pos" | cut -d' ' -f1)
-		star_x=$(echo "$pos" | cut -d' ' -f2)
-
-		if ((RANDOM % 2 == 0)); then
-			# Show a twinkling star
-			tput cup "$star_y" "$star_x"
-			echo -n "${color_twinkle}*"
-		else
-			# Clear the position
-			tput cup "$star_y" "$star_x"
-			echo -n " "
-		fi
-	done
 
 	# display the text
 	y=$((middle_y + TREE_HEIGHT + 1))
@@ -133,10 +86,5 @@ while true; do
 	# increment the lights and pause for the animation to play
 	((idx++))
 
-	# Regenerate star positions occasionally to vary the twinkling
-	if ((idx % 10 == 0)); then
-		generate_star_positions
-	fi
-
-	sleep 0.4
+	sleep 1
 done
